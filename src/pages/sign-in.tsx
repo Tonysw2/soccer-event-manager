512380be499dfc1b113b3809c08bee8e67631469
Anthony Ribeiro da Mata <anthonyrmataa@gmail.com>
2024-07-29 23:24:28 -0300

    update google scopes

--- src/pages/sign-in.tsx
+++ src/pages/sign-in.tsx
@@ -22,7 +22,8 @@ export function SignIn() {
       client_id: env.VITE_GOOGLE_CLIENT_ID,
       redirect_uri: 'http://localhost:5173/callbacks/google',
       response_type: 'token',
-      scope: '',
+      scope:
+        'https://www.googleapis.com/auth/calendar https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile',
     })
   }
 
